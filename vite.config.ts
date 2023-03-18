9108b52a8f8064359b4af9d369dd0fcce888ea6a
Noam Honig <noam.honig@gmail.com>
2023-03-18 09:08:49 +0200

    fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://noam-honig/github.io/wikiloc/",
+  base:"https://noam-honig.github.io/wikiloc/",
   plugins: [react()],
 })
