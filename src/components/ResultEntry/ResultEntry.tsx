95618ca1693aa5ed26120952f1941659423f7643
Noam Honig <noam.honig@gmail.com>
2023-03-20 16:17:13 +0200

    fixed problem on ios

--- src/components/ResultEntry/ResultEntry.tsx
+++ src/components/ResultEntry/ResultEntry.tsx
@@ -45,37 +45,41 @@ const ResultEntry: FC<ResultEntryProps> = ({ result, location }) => {
             </div>
             {result.wikiLang !== "he" && (
               <div>
-                <a href={"https://translate.google.com/translate?js=n&sl=en&tl=he&u="+getResultLink(result)} target="_blank">
+                <a
+                  href={
+                    "https://translate.google.com/translate?js=n&sl=en&tl=he&u=" +
+                    getResultLink(result)
+                  }
+                  target="_blank"
+                >
                   תרגום של גוגל
                 </a>
               </div>
             )}
           </div>
-          <div className="NewResultEntry--location-area">
-            <a
-              href={getGoogleMapLink(result)}
-              target="_blank"
-              className={"google-link"}
+          <a
+            className="NewResultEntry--location-area"
+            href={getGoogleMapLink(result)}
+            target="_blank"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-                />
-              </svg>
-            </a>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+              />
+            </svg>
             <div className="ResultEntry--main__distance">
               {(result?.dist / 1000)?.toFixed(1)}{" "}
               <span className={"ResultEntry--main__distance--text"}>ק"מ</span>
@@ -83,7 +87,7 @@ const ResultEntry: FC<ResultEntryProps> = ({ result, location }) => {
             <div style={{ fontSize: "small" }}>
               {direction(location, result)}
             </div>
-          </div>
+          </a>
         </div>
       </div>
     </div>
