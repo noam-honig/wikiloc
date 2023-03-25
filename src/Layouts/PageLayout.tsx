9fe2afb0f4b58c58aa2a34f5eeea96404ceadc4b
develad <develadb@gmail.com>
2023-03-26 01:47:12 +0300

    Context Added! - in progress- Adding Header component in PageLayout.tsx

--- src/Layouts/PageLayout.tsx
+++ src/Layouts/PageLayout.tsx
@@ -1,3 +1,4 @@
+import Header from "../components/Header/Header";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 import "./pageLayout.css";
@@ -9,13 +10,7 @@ type Props = {
 function PageLayout({ children }: Props) {
   return (
     <div className="container">
-      <h2 id="top-header">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Wikipedia_Logo_Puzzle_Globe_Spins_Horizontally%2C_Revealing_The_Contents_Of_All_Of_Its_Puzzle_Pieces_Without_Background.gif"
-          alt="wikipedia"
-        />
-        ויקיפדיה סביבי
-      </h2>
+      <Header />
       {children}
       <NavBar />
       <Footer />
